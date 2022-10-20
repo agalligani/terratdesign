b3e63a12f4c3f29243556ed6a91c6ad27ee24a8f
Anthony Galligani <tony@MacBook-Pro.hsd1.nj.comcast.net>
2022-10-20 15:07:14 -0400

    Nav work

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import React from "react";
 import {Container, Row, Col} from "react-bootstrap";
+import DesignNav from "./components/Navbar/DesignNav";
 import './styles/App.css';
 
 const App = () =>{
 
     return  (
         <div>
-            <Container className="menu">
+            <Container>
+                <DesignNav></DesignNav>
+            </Container>
+            <Container className="main">
                 <Row className="rows">
-                    <Col className="columns hotel">
-                        <h3>Hotel</h3>
-                    </Col>
-                    <Col className="columns hotel">
-                        <h3>Restaurant</h3>
-                    </Col>
-                    <Col className="columns hotel">
-                        <h3>Residential</h3>
+                <Col className="columns hotel">
+                    <p className="text-center">
+                    <h3>Hotel</h3>
+                        <img src="https://via.placeholder.com/300x440" alt="Hotel"></img>
+                    </p>
+                </Col>
+                <Col className="columns restaurant">
+                    <p className="text-center">
+                    <h3>Restaurant</h3>
+                            <img src="https://via.placeholder.com/300x440" alt="Restaurant"></img>
+                    </p>
+                </Col>
+                <Col className="columns residential">
+                    <p className="text-center">
+                    <h3>Residential</h3>
+                        <img src="https://via.placeholder.com/300x440" alt="Residential"></img>
+                    </p>
                     </Col>
                 </Row>
             </Container>
