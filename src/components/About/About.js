dbaa0cd1168017c808f69d678b0c994a51c3b987
Anthony Galligani <tony@MacBook-Pro.hsd1.nj.comcast.net>
2022-11-18 07:56:24 -0500

    fixed image names - add About component

---
+++ src/components/About/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <p>Terrat Design is a small but capable boutique design firm. 
+                
+With over 20 years of interior design experience, our projects are as unique as our many clients. We ask, listen and deliver personalized interiors that suit a wide range of aesthetic and practical requirements. Never cookie-cutter, always personalized and appropriate.  
+
+As just a two person firm, we only take on a handful of projects so we can focus intensely on our clients needs and goals. We work with a talented team of craftspeople, vendors & trades and manage all the phases of a project from initial inspiration to the final installation. 
+
+Our goal is to deliver spaces our clients will love looking at, and living in, for years to come.
+</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
