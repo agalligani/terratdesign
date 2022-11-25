d9a4cd5ccf36ec128c2895c02a1fa4a95eaeaac0
Anthony Galligani <tony@MacBook-Pro.hsd1.nj.comcast.net>
2022-11-25 15:05:05 -0500

    forgot to commit public/main.js?

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,28 +4,30 @@ import {Container, Row, Col} from "react-bootstrap";
 const Home = () => { 
 
     return (
+    <React.Fragment>        
     <Container className="main">
-        <ul className="main-menu">
-            <li className="menu-item hotel text-center">
+        <Row className="home-row">
+            <Col className="home-column hotel text-center">
                 <a href="#/portfolio/hotel">
-                    <h3>Hotel</h3>
+                    <h3><span className="redred">HOT</span>EL</h3>
                     <img src="/images/menu/menu_001.jpg" alt="Terrat Designs - Hotel Interiors"></img>
                 </a>
-           </li>
-            <li className="menu-item restaurant text-center">
+           </Col>
+            <Col className="home-column restaurant text-center">
                 <a href="#/portfolio/restaurant">
-                    <h3>Restaurant</h3>
+                    <h3><span className="redred">REST</span>AURANT</h3>
                     <img src="/images/menu/menu_002.jpg" alt="Terrat Designs - Restaurant Interiors"></img>
                 </a>
-            </li>
-            <li className="menu-item residential text-center">
+            </Col>
+            <Col className="home-column residential text-center">
                 <a href="#/portfolio/residential">
-                    <h3>Residential</h3>
+                    <h3><span className="redred">RESI</span>DENTIAL</h3>
                     <img src="/images/menu/menu_003.jpg" alt="Terrat Designs - Residential Interiors"></img>
                 </a>
-            </li>
-        </ul>
+            </Col>
+        </Row>
     </Container>
+    </React.Fragment>
     );
 }
 
--- src/components/Home/HomeBad.js
+++ src/components/Home/HomeBad.js
@@ -4,30 +4,28 @@ import {Container, Row, Col} from "react-bootstrap";
 const Home = () => { 
 
     return (
-    <React.Fragment>        
     <Container className="main">
-        <Row className="rows">
-            <Col className="columns hotel text-center">
+        <ul className="main-menu">
+            <li className="menu-item hotel text-center">
                 <a href="#/portfolio/hotel">
                     <h3>Hotel</h3>
                     <img src="/images/menu/menu_001.jpg" alt="Terrat Designs - Hotel Interiors"></img>
                 </a>
-           </Col>
-            <Col className="columns restaurant text-center">
+           </li>
+            <li className="menu-item restaurant text-center">
                 <a href="#/portfolio/restaurant">
                     <h3>Restaurant</h3>
                     <img src="/images/menu/menu_002.jpg" alt="Terrat Designs - Restaurant Interiors"></img>
                 </a>
-            </Col>
-            <Col className="columns residential text-center">
+            </li>
+            <li className="menu-item residential text-center">
                 <a href="#/portfolio/residential">
                     <h3>Residential</h3>
                     <img src="/images/menu/menu_003.jpg" alt="Terrat Designs - Residential Interiors"></img>
                 </a>
-            </Col>
-        </Row>
+            </li>
+        </ul>
     </Container>
-    </React.Fragment>
     );
 }
 
