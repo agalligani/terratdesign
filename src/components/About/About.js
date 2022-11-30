4583d577f74c8afbed8848cb2d2e34053b77e3f7
Anthony Galligani <anthony.galligani@gmail.com>
2022-11-30 11:37:09 -0500

    styling for small and under

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,7 +18,7 @@ const About = () => {
             </header>
             <article>
                 <div className="about-page-article">
-                    <p><b>Terrat <span className="redred">Design</span></b> is a small but capable boutique design firm.</p>
+                    <p><div className="call-out">Terrat <span className="redred">Design</span></div> is a small but capable boutique design firm.</p>
                     <p>With over 20 years of interior design experience, our projects are as unique as our many clients. We ask, listen and deliver personalized interiors that suit a wide range of aesthetic and practical requirements. Never cookie-cutter, always personalized and appropriate.</p>    
                     <p>As just a two person firm, we only take on a handful of projects so we can focus intensely on our clients needs and goals. We work with a talented team of craftspeople, vendors & trades and manage all the phases of a project from initial inspiration to the final installation.</p>
                     <p>Our goal is to deliver spaces our clients will love looking at, and living in, for years to come.</p>
